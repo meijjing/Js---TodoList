e80d65848a6c9be41672800336adf6865e2cb6b0
meijjing <kbunny711@naver.com>
2021-08-13 15:49:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JavaScript TodoList
+
+## 순수 자바스크립트로 만든 To Do List
+
+* localStorage를 사용한 list 저장 기능 구현
+* refresh button 클릭 시, 새로고침
+* addEventListener 이벤트 동시발생 기능 구현
+* todo-list 클릭 시, todo의 상태 변경
---
+++ js/script.js
@@ -0,0 +1,139 @@
+// script.js
+
+// select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const form = document.querySelector(".add-item");
+const add = document.getElementById("add");
+const input = document.getElementById("input");
+const item = document.querySelector(".item");
+const check = document.querySelector(".check");
+const remove = document.querySelector(".delete");
+const text = document.querySelector(".text");
+
+// class name
+const CHECK = "fa-check";
+// const UNCHECK = "check";
+const LINE_THROUGH = "line-through";
+
+// variables
+let LIST, id;
+
+// get item from local storage --------------------------
+let data = localStorage.getItem("todo");
+
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadToDo(LIST);
+} else {
+  LIST = [];
+  id = 0;
+}
+
+function loadToDo(array) {
+  array.forEach(function (item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+
+// clear local storage -------------------------------------
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+
+// show Date ----------------------------------------
+let today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("ko-KR", {
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+  weekday: "short",
+});
+
+// add a to-do item ----------------------------------
+// element.insertAdjacentHTML(position, text);
+// "beforebegin", "afterbegin", "beforeend", "afterend"
+// element.innerHTML(text);
+
+function addToDo(todo, id, done, trash) {
+  if (trash) return;
+
+  const DONE = done ? CHECK : "";
+  const LINE = done ? LINE_THROUGH : "";
+
+  const text = `<li class="item">
+    <i class="check fa ${DONE}" job="done" id="${id}"></i>
+    <p class="text ${LINE}">${todo}</p>
+    <i class="fa fa-trash delete" job="remove" id="${id}"></i></li>`;
+  const position = "beforeend";
+
+  list.insertAdjacentHTML(position, text);
+}
+
+// enter key ------------------------------------------
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const todo = input.value;
+
+  // input isn't empty
+  if (todo) {
+    addToDo(todo, id, false, false);
+    LIST.push({
+      name: todo,
+      id: id,
+      done: false,
+      trash: false,
+    });
+
+    // add item to local storage
+    localStorage.setItem("todo", JSON.stringify(LIST));
+
+    id++;
+  }
+  input.value = null;
+});
+
+// to-do is Done ---------------------------------------
+function todoDone(element) {
+  console.log(element);
+
+  element.classList.toggle(CHECK);
+  // element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove to-do ---------------------------------------
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
+}
+
+// target an element created dynamically
+list.addEventListener("click", function (event) {
+  const element = event.target;
+  const elementJOB = event.target.attributes.job.value; // remove or done
+
+  if (elementJOB == "done") {
+    todoDone(element);
+  } else if (elementJOB == "remove") {
+    removeToDo(element);
+  }
+});
+
+// bg random change
+const bgArr = new Array();
+bgArr[0] = "../img/bg-1.jpg";
+bgArr[1] = "../img/bg-2.jpg";
+bgArr[2] = "../img/bg-3.jpg";
+bgArr[3] = "../img/bg-4.jpg";
+bgArr[4] = "../img/bg-5.jpg";
+
+function showBg() {
+  const bgNum = Math.round(Math.random() * 1);
+}
